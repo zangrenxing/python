55dccecdac8dda2ea877370679483eaa8cdb1b3f
zangrenxing <715278611@qq.com>
2025-06-13 08:23:58 +0800

    更新小米长滚动.py，修改滑动参数并调整随机等待时间；新增小米10秒划一次.py和小米长划.py文件

---
+++ 小米10秒划一次.py
@@ -0,0 +1,7 @@
+import os
+import time
+import random
+for i in range(0, 2):
+    os.system('adb -s 79e8d0f8 shell input swipe 500 1600 500 300 400')
+    time.sleep(random.randint(10,10))
+os.system('adb -s 79e8d0f8 shell input swipe 500 1600 500 300 400')
--- 小米长滚动.py
+++ 小米长滚动.py
@@ -2,5 +2,7 @@ import os
 import time
 import random
 for i in range(0, 2000):
-    os.system('adb -s 79e8d0f8 shell input swipe 500 1600 500 300 300')
-    time.sleep(random.randint(8, 9))
+    # os.system('adb -s 79e8d0f8 shell input swipe 500 1600 500 300 300')
+    os.system('adb -s 79e8d0f8 shell input swipe 500 1000 500 300 300')
+    # time.sleep(random.randint(8, 9))
+    time.sleep(random.randint(8, 19))
